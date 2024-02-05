1bc6b2d2050aa3a0be49991fdacd9863ae9eb1ba
Abdelghafour <ghafourragg@gmail.com>
2024-02-05 22:08:19 +0100

    added react-hook-form, fonts file, starter homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,10 @@ import { FaHeart } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="footer w-full flex flex-col">
+    <div className="footer w-full flex flex-col py-2">
       <p className="flex items-center justify-center">
-        Built with <FaHeart color="red" /> by @Abdelghafour122
+        {/* use your username as a link after making your website */}
+        Built with &nbsp; <FaHeart color="red" /> &nbsp; by @Abdelghafour122
       </p>
     </div>
   );
