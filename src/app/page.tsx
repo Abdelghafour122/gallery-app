daada2b116b0c2f76ef71adedda7e6701b28c8ee
Abdelghafour <ghafourragg@gmail.com>
2024-02-14 22:31:54 +0100

    made basic layout fix, making sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LondrinaSolid } from "./utils/fonts";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-between gap-14 px-2 mx-auto container">
+    <main className="flex h-fit flex-col items-center justify-between gap-14 px-2 mx-auto container">
       <div className="text flex py-2 items-center justify-center gap-6 flex-col">
         <h1 className={`${LondrinaSolid.className} text-9xl text-center`}>
           Welcome to Gallerina
